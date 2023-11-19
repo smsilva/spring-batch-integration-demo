485d45ac58626361e78db55a0d98a39a3333277f
Silvio Silva <smsilva@gmail.com>
2023-11-19 17:00:48 -0300

    feat: Transformation

--- src/main/java/com/github/smsilva/batch/integration/demo/transformers/CustomerTransformer.java
+++ src/main/java/com/github/smsilva/batch/integration/demo/transformers/CustomerTransformer.java
@@ -1,20 +1,13 @@
 package com.github.smsilva.batch.integration.demo.transformers;
 
 import com.github.smsilva.batch.integration.demo.domain.Customer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.integration.transformer.AbstractTransformer;
-import org.springframework.messaging.Message;
+import org.springframework.integration.core.GenericTransformer;
 
-public class CustomerTransformer extends AbstractTransformer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerTransformer.class);
+public class CustomerTransformer implements GenericTransformer<String, Customer> {
 
     @Override
-    protected Customer doTransform(Message<?> message) {
-        String payload = (String) message.getPayload();
-
-        String[] fields = payload.split(",");
+    public Customer transform(String line) {
+        String[] fields = line.split(",");
 
         Customer customer = new Customer();
         customer.setId(fields[0]);
--- src/main/java/com/github/smsilva/batch/integration/demo/transformers/CustomerTransformerConfig.java
+++ src/main/java/com/github/smsilva/batch/integration/demo/transformers/CustomerTransformerConfig.java
@@ -1,8 +1,10 @@
 package com.github.smsilva.batch.integration.demo.transformers;
 
+import com.github.smsilva.batch.integration.demo.domain.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.core.GenericTransformer;
 
 @Configuration
 public class CustomerTransformerConfig {
@@ -11,7 +13,7 @@ public class CustomerTransformerConfig {
     @Transformer(
             inputChannel = "customerTransformChannel",
             outputChannel = "inputCustomers")
-    public CustomerTransformer customerTransformer() {
+    public GenericTransformer<String, Customer> stringToCustomerTransformer() {
         return new CustomerTransformer();
     }
 
