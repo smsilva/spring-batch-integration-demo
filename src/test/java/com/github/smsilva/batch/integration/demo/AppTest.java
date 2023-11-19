170ab53e5f1a0889fd1220c7ae0080bd3fff58a6
Silvio Silva <smsilva@gmail.com>
2023-11-19 11:20:34 -0300

    feat: Initial git setup

---
+++ src/test/java/com/github/smsilva/batch/integration/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.smsilva.batch.integration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
