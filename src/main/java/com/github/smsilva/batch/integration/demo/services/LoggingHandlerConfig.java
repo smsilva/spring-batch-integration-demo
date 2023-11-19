f73a03198aee9a499ae55c7aa151207f80bfb1f8
Silvio Silva <smsilva@gmail.com>
2023-11-19 15:43:14 -0300

    feat: Configure a simple flow using FileInboundChannelAdapter

--- src/main/java/com/github/smsilva/batch/integration/demo/services/LoggingHandlerConfig.java
+++ src/main/java/com/github/smsilva/batch/integration/demo/services/LoggingHandlerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.messaging.Message;
 
 @Configuration
 public class LoggingHandlerConfig {
